431ce386ae3b32318e130fadbf7b7b947f2c3728
Carlos Villen <carlosvillen@gmail.com>
2021-06-06 19:05:31 +0100

    Modified navbar and removed photos.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,6 +21,10 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
   private
 
   def list_params
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'cvillen'
+  config.api_key = '877835815546698'
+  config.api_secret = '7BcfivJ9uCZ545Vz-XgnGbnM_r8'
+  config.secure = true
+  config.cdn_subdomain = true
+end
