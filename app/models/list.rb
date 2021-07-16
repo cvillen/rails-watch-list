19c0701e3de88c2632049893112713022030066b
Carlos Villen <carlosvillen@gmail.com>
2021-07-16 11:32:05 +0100

    Amended form to make a list to pick the name instead of type it in manually.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
+  CATEGORIES = %w[Drama Comedy Classic Favourites Horror Sci-fi Action Thriller Documentary Animation Musical Western Romance Biography]
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_one_attached :photo
